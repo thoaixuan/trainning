a76697230e16444d5eed000a7e357f97b97b8692
Son Thanh <coderthanhson@gmail.com>
2022-02-28 19:57:13 +0700

    làm bài jquery

---
+++ bai_4/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.img-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+});
+
+$(document).ready(function () {
+    $('.img-mul-slider').slick({
+        slidesToShow: 3,
+
+    });
+});
