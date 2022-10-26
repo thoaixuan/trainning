8f0a4098267ea807db18e787ad03f0c17718cb00
khoanguyenminh222 <khoanguyenminh222@gmail.com>
2022-10-26 16:44:06 +0700

    phần giao diện trang chủ

---
+++ Khoa_CatGiaoDien/main.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+$('#btnCollapse').click(()=>{
+  $('#sidebarContent').css('z-index',9999)
+  $('#sidebarContent').width(300)
+  $('#sidebarContent').height('100%')
+  $('#sidebarContent').css('top',0)
+  $('#sidebarContent').css('overflowY','auto')
+})
+$('#btn-close-sidebar').click(()=>{
+  $('#sidebarContent').removeClass('show')
+})
+$(function(){
+  $('.menu-sidebar').hover(function(){
+    $(this).css('background-color','rgba(0,0,0,0.05)')
+  },
+  function(){
+    $(this).css('background-color','#f8f9fa')
+  })
+})
