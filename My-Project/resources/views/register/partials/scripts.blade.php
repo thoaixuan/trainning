e1b05ae5950146af2b30e98ad0c5a9384456e76f
pathernduy <85820607+pathernduy@users.noreply.github.com>
2023-12-09 08:51:54 +0700

    delete old files

--- My-Project/resources/views/register/partials/scripts.blade.php
+++
@@ -1,4 +0,0 @@
-{{-- Jquery --}}
-<script src="{{url('themes//assets/js/jquery.min.js')}}"></script>
-{{-- Jquery-validate --}}
-<script src="{{url('themes//assets/js/jquery.validate.min.js')}}"></script>
--- My-Project/resources/views/register/partials/stylesheet.blade.php
+++
@@ -1,5 +0,0 @@
-{{-- icon-font --}}
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-<!-- Style css -->
-<link href="{{url('themes/assets/css/style.css')}}" rel="stylesheet" />
-
